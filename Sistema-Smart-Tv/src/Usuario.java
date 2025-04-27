390df444c327cde7e20c9ac5e84da5b90effdf1f
Vanessa <nessalbino@gmail.com>
2025-04-27 10:19:33 -0300

    Exercícios realizados nas aulas de Operadores e Metodos

---
+++ Sistema-Smart-Tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        
+        smartTv.ligar();
+        System.out.println("Nova Status -> TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.desligar();
+        System.out.println("Nova Status -> TV Ligada? " + smartTv.ligada);
+
+    }
+}
---
+++ Tipos-Variaveis/src/Operadores.java
@@ -0,0 +1,89 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        
+//        String nomeCompleto = "Linguagem" + "JAVA";
+//        System.out.println(nomeCompleto);
+
+//        String concatenacao = "?";
+        
+//      concatenacao = 1 + 1 + 1 + "1";
+//      System.out.println(concatenacao);
+
+//      concatenacao = 1 + "1" + 1 + 1;
+//      System.out.println(concatenacao);
+
+//      concatenacao = 1 + "1" + 1 + "1";
+//      System.out.println(concatenacao);
+
+//      concatenacao = "1" + 1 + 1 + 1;
+//      System.out.println(concatenacao);
+
+//      concatenacao = "1" + (1 + 1 + 1);
+//      System.out.println(concatenacao);
+//***********************************************/
+        // int numero =5;
+
+        // numero = - numero;
+        // System.out.println(numero);
+
+        // numero = - numero;
+        // System.out.println(numero);
+//***********************************************/
+        // int numero = 5;
+        // // x repetição
+
+        // // numero = numero + 1;
+        // numero ++;
+        // System.out.println(numero);
+/***********************************************/
+        // boolean variavel = true;
+
+        // System.out.println(!variavel);
+        // System.out.println(variavel);
+/***********************************************/
+        // int a, b;
+
+        // a = 6;
+        // b = 6;
+        
+        // String resultado = "";
+        // if(a==b)
+        // resultado = "verdadeiro";
+        // else    
+        // resultado = "false";
+
+        // String resultado = a==b ? "verdadeiro" : "falso";
+        // int resultado = a==b ? 1 : 0;
+
+        // System.out.println(resultado);
+/***********************************************/       
+        // int numero1 = 1;
+        // int numero2 = 2;
+
+        // boolean simNao = numero1 == numero2;
+
+        // System.out.println("numeroUm é igual a numeroDois? " + simNao);
+/***********************************************/
+        // String nomeUm = "Vanessa";
+        // // String nomeDois = "Vanessa";
+        // String nomeDois = new String ("Vanessa");
+
+        // // System.out.println(nomeUm == nomeDois);
+        // System.out.println(nomeUm.equals(nomeDois));
+/***********************************************/
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        // if (condicao1 && condicao2) {
+        if (condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras");
+        }
+        if (condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("FIM");
+    }
+    
+}
